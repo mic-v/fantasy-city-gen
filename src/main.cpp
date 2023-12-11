24fa79047b899f199e991cd3a75afb48b3176b83
mic-v <michaelvictorino@gmail.com>
2023-12-10 23:08:36 -0500

    Added SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hello, World!" << "\n";
+int main() {
+  auto window = sf::RenderWindow{{1920u, 1080u}, "CMake SFML Project"};
+  window.setFramerateLimit(144);
+
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event);) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
 }
